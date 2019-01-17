81e9fba8a3de29b4ce035802a1a252d2fb53ac00
shadiao <1498800286@qq.com>
2019-01-17 08:42:33 +0800

    first init

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 
 int main(){
+    int index =0;
 
-    printf("hello world!");
+    printf("jinrumingganzifuguanlixitong\n");
+    while(1){
+        printf("1---tianjiamingganzifu\n");
+        printf("2---chanchumingganzifu\n");
+        printf("3---suoyoumingganzifu\n");
+        printf("4---tihuanmingganzifu\n");
+        printf("5---tuichu\n");
+
+        printf("qingshuruxiangyinggongnengbianhao\n");
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+              printf("qingshuruyaotianjiademingganzifu\n");
+              char m;
+              scanf("%c",&m);
+              scanf("%c",&m);
+             
+              printf("tianjiachenggong\n");
+              char x;
+              scanf("%c",&x);
+              scanf("%c",&x);
+        }
+         if(code == 2){
+            
+        }
+         if(code == 3){
+            
+        }
+         if(code == 4){
+            
+        }
+         if(code == 5){
+            
+        }
+    }
 
     return 0;
 
